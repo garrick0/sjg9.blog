0177f543a9c7e82db3db4d0960b9eff1c54680ae
samuel gleeson <samuel.gleeson@gmail.com>
2021-04-11 17:35:00 +1000

    Added first post in source, create-react-app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DAY ONE: POST ONE
+          A challenge for this week. Each day I will create and publish something, whether some writing or a creation. Like this, it will be scrappy. I'm hoping to learn both subject matter, improve my accountability and overcome the fear of starting. 
+        </p>
+
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
